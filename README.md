d1180a93b14a75c6f1c8c0786618ab0bf07a16c9
kas-alt <arpanx980@gmail.com>
2025-05-14 16:34:28 +1000

    drawing @ in console

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lcs_dma
 Just using tcod lib in python
+
+. new update: py -m pip install attrs
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
 
+import attrs
 import tcod.console
 import tcod.context
 import tcod.event
 import tcod.tileset
+
+
+@attrs.define()
+class ExampleState:
+    """Example state with a hard-coded player position."""
+
+    player_x: int
+    """Player X position, left-most position is zero."""
+    player_y: int
+    """Player Y position, top-most position is zero."""
+
+    def on_draw(self, console: tcod.console.Console) -> None:
+        """Draw the player glyph."""
+        console.print(self.player_x, self.player_y, "@")
 
 
 def main() -> None:
@@ -14,12 +29,16 @@ def main() -> None:
     tcod.tileset.procedural_block_elements(tileset=font_tileset)
     
     # setting window console
-    console = tcod.console.Console(80, 50)
-    console.print(0, 0, "Hello World") # x, y, text
+    console = tcod.console.Console(64, 48)
+    # console.print(0, 0, "Hello World") # x, y, text
+    state = ExampleState(player_x=console.width // 2, player_y=console.height // 2)
     
     with tcod.context.new(console = console , tileset=font_tileset) as context:
         while True: # main loop
-            context.present(console)
+            console.clear()  # Clear the console before any drawing
+            state.on_draw(console)  # Draw the current state
+            context.present(console)  # Display the console on the window
+            # context.present(console)
             for event in tcod.event.wait():
                 print(event)
             
