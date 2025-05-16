ffcb576c192434094e487495421e54075bfef42c
kas-alt <arpanx980@gmail.com>
2025-05-16 12:34:22 +1000

    controller on_event.state

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ import tcod.tileset
 
 @attrs.define()
 class ExampleState:
-    """Example state with a hard-coded player position."""
 
     player_x: int
-    """Player X position, left-most position is zero."""
     player_y: int
-    """Player Y position, top-most position is zero."""
 
     def on_draw(self, console: tcod.console.Console) -> None:
-        """Draw the player glyph."""
         console.print(self.player_x, self.player_y, "@")
+
+    def on_event(self, event: tcod.event.Event)-> None:
+        """Movement for player"""
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit
+            case tcod.event.KeyDown(sym= tcod.event.KeySym.LEFT):
+                self.player_x -=1
+            case tcod.event.KeyDown(sym= tcod.event.KeySym.RIGHT):
+                self.player_x +=1
+            case tcod.event.KeyDown(sym= tcod.event.KeySym.UP):
+                self.player_y -=1
+            case tcod.event.KeyDown(sym= tcod.event.KeySym.DOWN):
+                self.player_y +=1
 
 
 def main() -> None:
@@ -31,20 +41,24 @@ def main() -> None:
     # setting window console
     console = tcod.console.Console(64, 48)
     # console.print(0, 0, "Hello World") # x, y, text
-    state = ExampleState(player_x=console.width // 2, player_y=console.height // 2)
+    state = ExampleState(player_x = console.width // 2, player_y = console.height // 2)
     
     with tcod.context.new(console = console , tileset=font_tileset) as context:
         while True: # main loop
+            
             console.clear()  # Clear the console before any drawing
             state.on_draw(console)  # Draw the current state
             context.present(console)  # Display the console on the window
+
             # context.present(console)
             for event in tcod.event.wait():
                 print(event)
-            
+                state.on_event(event)
+
+
             # demo check
-            if isinstance(event, tcod.event.Quit):
-                raise SystemExit
+            # if isinstance(event, tcod.event.Quit):
+            #     raise SystemExit
             
         pass # end window here
 
